044808f9292f6c5df0f6297c54b58b715aba8d0b
anweshhhh <singh.anw@northeastern.edu>
2024-02-20 00:19:12 -0500

    UserLogin-JWT

--- server.js
+++ server.js
@@ -48,6 +48,34 @@ app.post('/register', async (req, res) => {
   }
 });
 
+// Login Endpoint
+app.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Find user by username
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid username or password" });
+    }
+
+    // Compare passwords
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ message: "Invalid username or password" });
+    }
+
+    // Generate JWT Token
+    const token = jwt.sign({ userId: user._id }, process.env.TOKEN_SECRET, { expiresIn: '1h' });
+
+    res.json({ accessToken: token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error logging in' + error.message);
+  }
+});
+
   
 // Protected Route
 app.get('/protected', verifyJWT, (req, res) => {
